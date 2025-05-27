46cb1374f8427347b3c92c9891a00870b52290da
maopo <markvincentopo@gmail.com>
2025-05-27 12:44:36 +0800

    Change deploy script and update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   app: {
     baseURL: "/portfolio/",
+    buildAssetsDir: "assets",
   },
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
