2f1674278481455fff16c02d3df32a23648151f5
maopo <markvincentopo@gmail.com>
2025-05-27 12:30:22 +0800

    Added deploy script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    baseURL: "/portfolio/",
+  },
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: [
