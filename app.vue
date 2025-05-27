a48c89ae876a44be3e6fcdfdfae4b4b4ec78b7e0
maopo <markvincentopo@gmail.com>
2025-05-27 13:48:34 +0800

    Update generated files

--- app.vue
+++ app.vue
@@ -1,7 +1,31 @@
 <template>
 	<v-app>
-		<NuxtPage></NuxtPage>
+		<Toolbar></Toolbar>
+
+		<v-container class="mt-5">
+			<div id="about" class="pt-16">
+				<About />
+			</div>
+
+			<v-divider class="mt-10" />
+
+			<div id="projects" class="pt-16">
+				<Projects />
+			</div>
+
+			<v-divider class="mt-10" />
+
+			<div id="experience" class="pt-16">
+				<Experience />
+			</div>
+		</v-container>
+
+		<Footer></Footer>
 	</v-app>
 </template>
 
-
+<style>
+html {
+	scroll-behavior: smooth;
+}
+</style>
