684ef2b7631f0cb8d8534118c3a225974a623d69
maopo <markvincentopo@gmail.com>
2025-05-27 12:52:10 +0800

    Update config and remove dist from ignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   app: {
     baseURL: "/portfolio/",
-    buildAssetsDir: "assets",
+  },
+  nitro: {
+    preset: "static",
   },
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
